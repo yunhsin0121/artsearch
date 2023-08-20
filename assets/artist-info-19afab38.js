50b2d6501d059e03203e6c387f2c6e69c092b504
yunhsin <alice334830@gmail.com>
2023-08-20 22:16:55 +0800

    Updates

--- assets/artist-info-19afab38.js
+++
@@ -1,1 +0,0 @@
-import"./main-6c1a32ff.js";$(document).ready(function(){$(".like").click(function(t){t.preventDefault(),$(this).toggleClass("fa-regular fa-heart"),$(this).toggleClass("fa-solid fa-heart")})});
