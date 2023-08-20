b3e697c76f35a09fb4fe2ed7bb35e7bbbd55c216
yunhsin <alice334830@gmail.com>
2023-08-20 22:17:18 +0800

    update jquery

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 import './assets/scss/all.scss';
+// import './assets/js/jquery.js';
 import 'bootstrap/dist/js/bootstrap.min.js';
-import'./assets/js/jquery';
 console.log("Hello world!");
-// 
+$(document).ready(function () {
+    $(".like").click(function (e) { 
+        e.preventDefault();
+        $(this).toggleClass('fa-regular fa-heart');
+        $(this).toggleClass('fa-solid fa-heart');
+    });
+});
