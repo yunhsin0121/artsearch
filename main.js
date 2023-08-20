707e521b7a6df0f753e25e446064b0e3a01ec2bb
yunhsin <alice334830@gmail.com>
2023-08-20 22:02:02 +0800

    jquery

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 console.log("Hello world!");
+$(document).ready(function () {
+    $(".like").click(function (e) { 
+        e.preventDefault();
+        $(this).toggleClass('fa-regular fa-heart');
+        $(this).toggleClass('fa-solid fa-heart');
+    });
+});
